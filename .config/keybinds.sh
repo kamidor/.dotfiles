63c0cf91c416d5186d8570fe9fe5029f8bfb5111
Tristan Strug <tstrug@ssystems.de>
2021-03-13 20:25:52 +0100

    Rebind capslock to escape

--- .config/keybinds.sh
+++ .config/keybinds.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 xmodmap -e "keycode 52 = y Y y Y less less"
 xmodmap -e "keycode 53 = x X x X greater greater"
-xmodmap -e "keycode 54= c C c C bar bar"
+xmodmap -e "keycode 54 = c C c C bar bar"
+xmodmap -e "keycode 66 = Escape"
