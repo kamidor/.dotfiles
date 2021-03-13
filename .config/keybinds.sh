03b1d7cad6d1557a4aa1a15470d81a5f056688c7
Tristan Strug <tstrug@ssystems.de>
2021-03-13 22:31:47 +0100

    Unbind capslock and rebind it to escape

--- .config/keybinds.sh
+++ .config/keybinds.sh
@@ -2,4 +2,5 @@
 xmodmap -e "keycode 52 = y Y y Y less less"
 xmodmap -e "keycode 53 = x X x X greater greater"
 xmodmap -e "keycode 54 = c C c C bar bar"
-xmodmap -e "keycode 66 = Escape"
+setxkbmap -option caps:none
+xmodmap -e "keycode 66 = Escape Escape Escape Escape Escape Escape" 
